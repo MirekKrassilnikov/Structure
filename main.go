4c4af7df8bb1b37d8c3dec3e35db52753b7de1a7
Miroslav Krassilnikov <mirekrassilnikov@gmail.com>
2024-04-24 11:09:47 +0200

    second iteration

--- main.go
+++ main.go
@@ -1,52 +1,122 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-type workers struct {
-
-	ID             int
-	Name           string
-	BirthDate      time.Time
-	HireDate       time.Time
-	IsEmployed     bool
-	Salary         float64
+type Worker struct {
+	Name      string
+	BirthDate time.Time
+	HireDate  time.Time
 }
-func () {
-	worker1 := workers{
-		ID:         1,
-		Name:       "Alex",
-		BirthDate:  parseDate("1990-01-15"),
-		HireDate:   parseDate("2022-03-20"),
-		IsEmployed: true,
-		Salary:     50000.0,
-	}
-	worker2 := workers{
-		ID:         2,
-		Name:       "John",
-		BirthDate:  parseDate("1993-02-11"),
-		HireDate:   parseDate("2020-02-01"),
-		IsEmployed: true,
-		Salary:     40000.0
-	}
-	worker3 := workers{
-		ID:         3,
-		Name:       "Tom",
-		BirthDate:  parseDate("1950-06-09"),
-		HireDate:   parseDate("2002-08-30"),
-		IsEmployed: false,
-		Salary:     20000.0
-	}
 
-	worker4 := workers{
-		ID:         4,
-		Name:       "Sam",
-		BirthDate:  parseDate("1978-09-13"),
-		HireDate:   parseDate("2010-06-05"),
-		IsEmployed: false,
-		Salary:     25000.0
+// запрашивает количество работников и создает слайс с структут с заданным количеством
+func createWorkersSlice() []Worker {
+	fmt.Println("Enter ammount of workers")
+	var workersAmmount int
+	fmt.Scan(&workersAmmount)
+
+	workers := make([]Worker, workersAmmount)
+
+	for i, _ := range *workers {
+		layout := "2006-01-02"
+		var name string
+		var birthdate string
+		var hiredate string
+		fmt.Println("Enter name")
+		fmt.Scan(&name)
+
+		fmt.Println("Enter birth date")
+		fmt.Scan(&birthdate)
+
+		fmt.Println("Enter hire date")
+		fmt.Scan(&hiredate)
+
+		(*workers)[i] = Worker{
+			Name:      name,
+			BirthDate: parseDateString(birthdate, layout),
+			HireDate:  parseDateString(hiredate, layout)}
 	}
-	printworkersInfo(worker1)
-	printworkersInfo(worker2)
-	printworkersInfo(worker3)
-	printworkersInfo(worker4)
+	return workers
 }
+
+// Заполняет информацию о каждом работнике
+
+/*
+	func fillStruct(*Workers) {
+		for i, _ := range *Workers {
+			layout := "2006-01-02"
+			var name string
+			var birthdate string
+			var hiredate string
+			fmt.Println("Enter name")
+			fmt.Scan(&name)
+
+			fmt.Println("Enter birth date")
+			fmt.Scan(&birthdate)
+
+			fmt.Println("Enter hire date")
+			fmt.Scan(&hiredate)
+
+			(*Workers)[i] = Worker{
+				Name:      name,
+				BirthDate: parseDateString(birthdate, layout),
+				HireDate:  parseDateString(hiredate, layout)}
+		}
+	}
+*/
+func parseDateString(dateString string, layout string) time.Time {
+	parsedDate, err := time.Parse(layout, dateString)
+	if err != nil {
+		panic(err)
+	}
+	return parsedDate
+}
+
+func main() {
+	Workers := createWorkersSlice()
+	fmt.Println("Workers:")
+	for _, worker := range Workers {
+		fmt.Printf("Name: %s, BirthDate: %s, HireDate: %s\n", worker.Name, worker.BirthDate.Format("2006-01-02"), worker.HireDate.Format("2006-01-02"))
+	}
+}
+
+/*
+	var workersList []workers
+	workersList = append(workersList, worker1, worker2, worker3, worker4)
+	firedWorkers(workersList)
+	salaryRate(workersList, 23000)
+	dateCompare(workersList, comparedDate)
+}
+
+func parseDateString(dateString string, layout string) time.Time {
+	parsedDate, err := time.Parse(layout, dateString)
+	if err != nil {
+		panic(err)
+	}
+	return parsedDate
+}
+func firedWorkers(workersList []workers) {
+	for _, worker := range workersList {
+		if !worker.IsEmployed {
+			fmt.Println(worker.Name)
+		}
+	}
+}
+func salaryRate(workersList []workers, salary float64) {
+	for _, worker := range workersList {
+		if worker.Salary < salary {
+			fmt.Println("Workers with salary higher than", salary, "are:", worker.Name)
+		}
+	}
+}
+
+func dateCompare(workersList []workers, comparedDate time.Time) {
+	for _, worker := range workersList {
+		if worker.HireDate.Before(comparedDate) {
+			fmt.Println("Workers hired before", comparedDate, "are:", worker.Name)
+		}
+	}
+}
+*/
