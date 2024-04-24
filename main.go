c287822de109896abdd9303820b456dd9ad16d2f
Miroslav Krassilnikov <mirekrassilnikov@gmail.com>
2024-04-24 15:58:51 +0200

    Change method to func

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+const layout = "2006-01-02"
+
 type Worker struct {
 	Name      string
 	BirthDate time.Time
@@ -18,31 +20,87 @@ func createWorkersSlice() []Worker {
 	fmt.Scan(&workersAmmount)
 
 	workers := make([]Worker, workersAmmount)
-
-	for i, _ := range *workers {
-		layout := "2006-01-02"
-		var name string
-		var birthdate string
-		var hiredate string
-		fmt.Println("Enter name")
-		fmt.Scan(&name)
-
-		fmt.Println("Enter birth date")
-		fmt.Scan(&birthdate)
-
-		fmt.Println("Enter hire date")
-		fmt.Scan(&hiredate)
-
-		(*workers)[i] = Worker{
-			Name:      name,
-			BirthDate: parseDateString(birthdate, layout),
-			HireDate:  parseDateString(hiredate, layout)}
-	}
 	return workers
 }
 
-// Заполняет информацию о каждом работнике
+func fillStruct(worker *Worker) {
 
+	for {
+		var name string
+		fmt.Println("Enter name")
+		fmt.Scan(&name)
+
+		// Проверка на пустую строку
+		if len(name) == 0 {
+			fmt.Println("Name cannot be empty, enter name again")
+			continue
+		}
+		worker.Name = name
+		break
+	}
+	for {
+
+		var birthdate string
+		fmt.Println("Enter birth date")
+		fmt.Scan(&birthdate)
+
+		parsedDate, err := parseDateString(birthdate, layout)
+		if err != nil {
+			fmt.Println("Invalid date format, enter birth date again ")
+			continue
+		}
+		worker.BirthDate = parsedDate
+		break
+	}
+
+	for {
+		var hiredate string
+		fmt.Println("Enter hire date")
+		fmt.Scan(&hiredate)
+		parsedDate, err := parseDateString(hiredate, layout)
+		if err != nil {
+			fmt.Println("Invalid date format, enter hire date again ")
+			continue
+		}
+		worker.HireDate = parsedDate
+		break
+	}
+}
+
+func parseDateString(dateString string, layout string) (time.Time, error) {
+	parsedDate, err := time.Parse(layout, dateString)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return parsedDate, nil
+}
+func dateCompare(workersList []Worker, comparedDate time.Time) {
+	for _, worker := range workersList {
+		if worker.HireDate.Before(comparedDate) {
+			fmt.Println("Workers hired before", comparedDate, "are:", worker.Name)
+		}
+	}
+}
+
+func main() {
+	Workers := createWorkersSlice()
+	for i := 0; i < len(Workers); i++ {
+		fillStruct(&Workers[i])
+	}
+
+	fmt.Println("Workers:")
+	for _, worker := range Workers {
+		fmt.Printf("Name: %s, BirthDate: %s, HireDate: %s\n", worker.Name, worker.BirthDate.Format(layout), worker.HireDate.Format(layout))
+	}
+	comparedDate, err := parseDateString("2007-01-01", layout)
+	if err != nil {
+		fmt.Println("Error parsing compared date:", err)
+		return
+	}
+	dateCompare(Workers, comparedDate)
+}
+
+// Заполняет информацию о каждом работнике
 /*
 	func fillStruct(*Workers) {
 		for i, _ := range *Workers {
@@ -66,22 +124,6 @@ func createWorkersSlice() []Worker {
 		}
 	}
 */
-func parseDateString(dateString string, layout string) time.Time {
-	parsedDate, err := time.Parse(layout, dateString)
-	if err != nil {
-		panic(err)
-	}
-	return parsedDate
-}
-
-func main() {
-	Workers := createWorkersSlice()
-	fmt.Println("Workers:")
-	for _, worker := range Workers {
-		fmt.Printf("Name: %s, BirthDate: %s, HireDate: %s\n", worker.Name, worker.BirthDate.Format("2006-01-02"), worker.HireDate.Format("2006-01-02"))
-	}
-}
-
 /*
 	var workersList []workers
 	workersList = append(workersList, worker1, worker2, worker3, worker4)
