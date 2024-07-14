dbf75e2aab9cf4b9fa53f6721f8b9cf1848ab8e9
Miroslav Krassilnikov <mirekrassilnikov@gmail.com>
2024-07-14 16:14:47 +0200

    add workers to database

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "modernc.org/sqlite"
 	"time"
 )
 
@@ -81,6 +83,32 @@ func dateCompare(workersList []Worker, comparedDate time.Time) {
 		}
 	}
 }
+func createWorkersTable(db *sql.DB) error {
+	createTableSQL := `
+    CREATE TABLE IF NOT EXISTS workers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        birthdate TEXT,
+        hiredate TEXT
+    );`
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		return fmt.Errorf("error creating workers table: %v", err)
+	}
+	return nil
+}
+
+func addWorkerToDatabase(db *sql.DB, workers []Worker) {
+	for _, worker := range workers {
+		_, err := db.Exec("INSERT INTO workers (name, birthdate, hiredate) VALUES (?, ?, ?)",
+			worker.Name, worker.BirthDate, worker.HireDate)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
 
 func main() {
 	Workers := createWorkersSlice()
@@ -98,67 +126,13 @@ func main() {
 		return
 	}
 	dateCompare(Workers, comparedDate)
-}
 
-// Заполняет информацию о каждом работнике
-/*
-	func fillStruct(*Workers) {
-		for i, _ := range *Workers {
-			layout := "2006-01-02"
-			var name string
-			var birthdate string
-			var hiredate string
-			fmt.Println("Enter name")
-			fmt.Scan(&name)
-
-			fmt.Println("Enter birth date")
-			fmt.Scan(&birthdate)
-
-			fmt.Println("Enter hire date")
-			fmt.Scan(&hiredate)
-
-			(*Workers)[i] = Worker{
-				Name:      name,
-				BirthDate: parseDateString(birthdate, layout),
-				HireDate:  parseDateString(hiredate, layout)}
-		}
-	}
-*/
-/*
-	var workersList []workers
-	workersList = append(workersList, worker1, worker2, worker3, worker4)
-	firedWorkers(workersList)
-	salaryRate(workersList, 23000)
-	dateCompare(workersList, comparedDate)
-}
-
-func parseDateString(dateString string, layout string) time.Time {
-	parsedDate, err := time.Parse(layout, dateString)
+	db, err := sql.Open("sqlite", "database_workers")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	return parsedDate
+	defer db.Close()
+	createWorkersTable(db)
+	addWorkerToDatabase(db, Workers)
 }
-func firedWorkers(workersList []workers) {
-	for _, worker := range workersList {
-		if !worker.IsEmployed {
-			fmt.Println(worker.Name)
-		}
-	}
-}
-func salaryRate(workersList []workers, salary float64) {
-	for _, worker := range workersList {
-		if worker.Salary < salary {
-			fmt.Println("Workers with salary higher than", salary, "are:", worker.Name)
-		}
-	}
-}
-
-func dateCompare(workersList []workers, comparedDate time.Time) {
-	for _, worker := range workersList {
-		if worker.HireDate.Before(comparedDate) {
-			fmt.Println("Workers hired before", comparedDate, "are:", worker.Name)
-		}
-	}
-}
-*/
